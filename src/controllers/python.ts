0dde5af5b53f1242ecf3225d0b81f2d043a94ce9
Jackie Li <li.jackie565@gmail.com>
2023-05-21 23:39:45 -0400

    Response Fix

--- src/controllers/python.ts
+++ src/controllers/python.ts
@@ -4,26 +4,29 @@ import { PythonShell } from "python-shell";
 import ref from "../config/MongoConfig";
 
 export default async function PythonScript(req: Request, res: Response) {
-  const collection = ref("users");
+  //const collection = ref("users");
   const { body } = req;
   fs.writeFileSync(`./client_data/${body.problem}/result.py`, body.code); //creates python file
-
   try {
     const pythonScript = await PythonShell.run(
       `./client_data/${body.problem}/driver.py`,
       {
         mode: "text",
         pythonOptions: ["-u"], // get print results in real-time
+        args: [""],
       }
     );
+
     for (let i = 0; i < pythonScript.length; i++) {
-      if (pythonScript[i] === "False") throw new Error();
+      if (pythonScript[i] === "False") {
+        console.log("error");
+      }
     }
+    res.status(200).send("moises is gay");
+    return;
   } catch (e) {
     console.log(e);
     res.status(400).send("Incorrect Solution");
+    return;
   }
-
-  // update profile
-  // send correct msg
 }
